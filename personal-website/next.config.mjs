906370fab6046b559d09555974f6c4f9b37681cc
kwabenadarkwa <darkwak@live.com>
2025-09-17 03:07:41 +0100

    adds api in mdx for adding footnotes to md

--- personal-website/next.config.mjs
+++ personal-website/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
 
 const withMDX = createMDX({
   options: {
-    remarkPlugins: [['remark-frontmatter'], ['remark-mdx-frontmatter']],
+    remarkPlugins: [['remark-frontmatter'], ['remark-mdx-frontmatter'],['remark-gfm']],
     rehypePlugins: [['rehype-katex', { strict: true, throwOnError: true }]],
   },
 });
